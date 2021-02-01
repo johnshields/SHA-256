fb15cffeee221bfe08e9a936a59d4569fca1f501
johnshields <G00348436@gmit.ie>
2021-02-01 14:39:06 +0000

    Update main.c

--- bit_operations/main.c
+++ bit_operations/main.c
@@ -15,11 +15,29 @@ void bin_print(int i) {
         k = ((1 << j) & i) ? 1 : 0;
         printf("%d", k);
     }
-
 }
 
 int main(int argc, char *argv[]) {
     int i = 0xf1;
+
+//    printf("Dec %\n", i);
+//    printf("Size of i: %d", sizeof(i));
+//    printf("Size of int: %d", sizeof(int));
+//    printf("Size of char: %d", sizeof(char));
+//
+//    char c = 41;
+//    printf("c in char is %c\n", c);
+//    printf("c in int is %d\n", c);
+//
+//    int j = 1000000000;
+//    printf("j int is %d\n", c);
+//    printf("j char is %c\n", (char) j);
+//    printf("j int from char is %d\n", (int) (char) j);
+
+//    for (unsigned  int i = 0: i <100000000000000000; i++);
+//    printf("1");
+//
+//    printf("\n");
 
     printf("Original:  ");
     bin_print(i);

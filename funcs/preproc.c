00353146044a1f967999beb754c3be8b8fec4ddf
johnshields <g00348436@gmit.ie>
2021-03-11 20:56:01 +0000

    While loop done for byte reading

--- funcs/preproc.c
+++ funcs/preproc.c
@@ -5,18 +5,17 @@
 
 int main(int argc, char *argv[]) {
     BYTE b;
-
     FILE *f;
-
     f = fopen(argv[1], "r");
-    size_t numBytes;
+    size_t numOfBytes;
+    numOfBytes = fread(&b, 1, 1, f);
 
-    numBytes = fread(&b, 1, 1, f);
-
-    while (numBytes) {
-        numBytes = fread(&b, 1, 1, f);
+    while (numOfBytes) {
+        printf("%c", b);
+        numOfBytes = fread(&b, 1, 1, f);
     }
     fclose(f);
+    printf("\n");
 
     return 0;
 }
